bdfb899a73ad30e7c276bf7e58c09f6adffd2f0a
aflores85 <aflores85@gmail.com>
2022-05-31 16:34:40 -0300

    main

---
+++ nuevovue/src/components/ListaForos.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+  <ul v-for="(foro, index) in foros" :key="index">
+    <li>{{ foro }}</li>
+  </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ListaForos",
+  data() {
+    return {
+      foros: [],
+    };
+  },
+  methods: {
+    getForos() {
+      fetch("http://localhost:5000/api/v1/getforo")
+        .then((response) => response.json())
+        .then((data) => {
+          this.foros = data.foros;
+        });
+    },
+  },
+  mounted() {
+    this.getForos();
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ nuevovue/src/views/InicioView.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div>
+  <hola-mundo-comp></hola-mundo-comp>
+  <lista-foros></lista-foros>
+  <form-foro></form-foro>
+  <Lista-Subject></Lista-Subject>>
+  </div>
+</template>
+
+<script>
+import HolaMundoComp from "../components/HolaMundoComp";
+import ListaForos from "../components/ListaForos";
+import FormForo from "../components/FormForo";
+import ListaSubject from "../components/ListaSubject";
+
+
+
+export default {
+  name: "InicioView",
+  components: { HolaMundoComp, ListaForos, FormForo },
+    
+};
+</script>
+
+<style scoped></style>
+
